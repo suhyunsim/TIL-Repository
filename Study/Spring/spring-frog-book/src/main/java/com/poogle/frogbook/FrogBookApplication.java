7b8de8eaecdaf93849b8294550dd12fb9ebbd14d
suhyunsim <cnc1106@naver.com>
2020-07-10 16:38:18 +0900

    [#3] chore: 프로젝트 생성

---
+++ Study/Spring/spring-frog-book/src/main/java/com/poogle/frogbook/FrogBookApplication.java
@@ -0,0 +1,13 @@
+package com.poogle.frogbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrogBookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FrogBookApplication.class, args);
+    }
+
+}
---
+++ Study/Spring/spring-frog-book/src/test/java/com/poogle/frogbook/FrogBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poogle.frogbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrogBookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

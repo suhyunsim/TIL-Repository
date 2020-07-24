a35a96908e3252167410d65d84de07082dbc741e
suhyunsim <cnc1106@naver.com>
2020-07-24 16:29:37 +0900

    [#3] feat: BoardController에 목록 기능 구현
    - 모델은 컨트롤러에서 뷰로 전달해주는 정보다.
    - 스프링 MVC에서 모델을 생성하는 것은 DispatcherServlet의 역할로 생성한 모델에 대한 참조 변수는,
     `@RequestMapping`어노테이션이 붙은 메서드에서 인자를 선언하기만 하면 자동을 받을 수 있다.
    - "board/list"라는 문자열 값을 반환하고 있는데 이를 이용해 DispatcherServlet이 사용자에게 보여줄 뷰를 선정한다.

---
+++ Study/Spring/heaven/src/main/java/com/heaven/mvc/board/controller/BoardController.java
@@ -0,0 +1,23 @@
+package com.heaven.mvc.board.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.heaven.mvc.board.service.BoardService;
+
+@Controller
+public class BoardController {
+	@Autowired
+	private BoardService boardService;
+	
+	@RequestMapping(value="/board/list")
+	public String list(Model model) {
+		model.addAttribute("boardList", boardService.list());
+		return "board/list";
+	}
+	
+
+}

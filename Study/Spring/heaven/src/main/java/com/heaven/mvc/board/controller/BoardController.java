6b4c2b07fe8a0daa79fec2a48796eadff4c2df5f
suhyunsim <cnc1106@naver.com>
2020-07-27 21:52:26 +0900

    [#3] feat: BoardController에 상세 기능 구현

--- Study/Spring/heaven/src/main/java/com/heaven/mvc/board/controller/BoardController.java
+++ Study/Spring/heaven/src/main/java/com/heaven/mvc/board/controller/BoardController.java
@@ -3,8 +3,8 @@ package com.heaven.mvc.board.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.heaven.mvc.board.service.BoardService;
 
@@ -17,6 +17,13 @@ public class BoardController {
 	public String list(Model model) {
 		model.addAttribute("boardList", boardService.list());
 		return "board/list";
+	}
+	
+	@RequestMapping(value="/board/read/{seq}")
+	public String read(Model model, @PathVariable int seq) {
+		model.addAttribute("boardVO", boardService.read(seq));
+		return "/board/read";
+
 	}
 	
 

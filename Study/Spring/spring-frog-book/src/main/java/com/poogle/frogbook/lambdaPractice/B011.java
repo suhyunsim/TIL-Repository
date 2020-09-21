cb071505c96a246cb48adfa424a401fd096876ec
suhyunsim <cnc1106@naver.com>
2020-09-21 23:48:40 +0900

    [#3] feat: 컬렉션 스트림에서 람다 사용 예제
    - 20세 미만 안되도록

---
+++ Study/Spring/spring-frog-book/src/main/java/com/poogle/frogbook/lambdaPractice/B011.java
@@ -0,0 +1,13 @@
+package com.poogle.frogbook.lambdaPractice;
+
+public class B011 {
+    public static void main(String[] args) {
+        Integer[] ages = {20, 25, 18, 27, 30, 21, 17, 19, 34};
+
+        for (int i = 0; i < ages.length; i++) {
+            if (ages[i] < 20) {
+                System.out.format("Age $d! Can't enter\n", ages[i]);
+            }
+        }
+    }
+}

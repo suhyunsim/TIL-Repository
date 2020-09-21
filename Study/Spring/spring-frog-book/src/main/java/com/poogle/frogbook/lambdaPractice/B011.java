e3046625984ae58b8cc6f41436e1c77846262165
suhyunsim <cnc1106@naver.com>
2020-09-22 00:10:49 +0900

    [#3] feat: 메서드 레퍼런스

--- Study/Spring/spring-frog-book/src/main/java/com/poogle/frogbook/lambdaPractice/B011.java
+++ Study/Spring/spring-frog-book/src/main/java/com/poogle/frogbook/lambdaPractice/B011.java
@@ -6,8 +6,7 @@ public class B011 {
     public static void main(String[] args) {
         Integer[] ages = {20, 25, 18, 27, 30, 21, 17, 19, 34};
 
-        Arrays.stream(ages)
-                .filter(age -> age < 20) // 20세 미만인 경우를 선별(Filter)
-                .forEach(age -> System.out.format("Age $d! Can't enter\n", age)); // 선별된 각 요소에 대해 입장 불가
+        Arrays.stream(ages).sorted().forEach(System.out::println);
+
     }
 }

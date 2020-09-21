3b74171909d042e5c707392a097079be3201b7d2
suhyunsim <cnc1106@naver.com>
2020-09-22 00:03:09 +0900

    [#3] refactor: 컬렉션 스트림 이용
    - 기존 배열을 이용해 스트림을 얻기 위해 Arrays 클래스의 stream() 정적 메서드를 사용
    - `filter()` 메서드는 SQL 구문에서 where 절 역할을 수행
    - 고객의 요구를 선언적으로 코딩할 수 있음. 의사소통 내용 자체가 그대로 코드로 구현 가능

--- Study/Spring/spring-frog-book/src/main/java/com/poogle/frogbook/lambdaPractice/B011.java
+++ Study/Spring/spring-frog-book/src/main/java/com/poogle/frogbook/lambdaPractice/B011.java
@@ -1,13 +1,13 @@
 package com.poogle.frogbook.lambdaPractice;
 
+import java.util.Arrays;
+
 public class B011 {
     public static void main(String[] args) {
         Integer[] ages = {20, 25, 18, 27, 30, 21, 17, 19, 34};
 
-        for (int age : ages) {
-            if (age < 20) {
-                System.out.format("Age $d! Can't enter\n", ages[i]);
-            }
-        }
+        Arrays.stream(ages)
+                .filter(age -> age < 20) // 20세 미만인 경우를 선별(Filter)
+                .forEach(age -> System.out.format("Age $d! Can't enter\n", age)); // 선별된 각 요소에 대해 입장 불가
     }
 }

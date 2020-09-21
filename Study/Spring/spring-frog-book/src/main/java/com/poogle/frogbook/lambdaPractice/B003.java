395a3af6180cfb82ebe6545a9d63ea0b2006e25b
suhyunsim <cnc1106@naver.com>
2020-09-21 22:06:31 +0900

    [#3] refactor: 람다에서 로직이 한 줄일 때 블록 기호 `{}` 생략 가능

--- Study/Spring/spring-frog-book/src/main/java/com/poogle/frogbook/lambdaPractice/B003.java
+++ Study/Spring/spring-frog-book/src/main/java/com/poogle/frogbook/lambdaPractice/B003.java
@@ -2,9 +2,7 @@ package com.poogle.frogbook.lambdaPractice;
 
 public class B003 {
     public static void main(String[] args) {
-        Runnable r = () -> {
-            System.out.println("Hello Lambda 3");
-        };
+        Runnable r = () -> System.out.println("Hello Lambda 3");
         r.run();
     }
 }

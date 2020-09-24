d25597b026b71b726ec813c24177f85f7e775ec4
suhyunsim <cnc1106@naver.com>
2020-09-24 23:23:05 +0900

    [#20] feat: 회원 서비스 개발
    - MemberService를 구현한다.
    - 회원 가입, 중복회원 검증, 전체 회원 조회 기능을 구현한다.

---
+++ Study/Spring/hello-spring/src/main/java/com/hello/hellospring/service/MemberService.java
@@ -0,0 +1,37 @@
+package com.hello.hellospring.service;
+
+import com.hello.hellospring.domain.Member;
+import com.hello.hellospring.repository.MemberRepository;
+import com.hello.hellospring.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    //회원가입
+    public Long join(Member member) {
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    //중복회원 검증
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> {
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+    }
+
+    //전체 회원 조회
+    public List<Member> findMembers() { //비즈니스에 가까운 메서드 명을 사용해야 함
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+
+}

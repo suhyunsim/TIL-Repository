9a90573da1c823a7e9f970856419deb538fa5df5
suhyunsim <cnc1106@naver.com>
2020-09-28 12:12:42 +0900

    [#20] refactor: 컴포넌트 스캔을 위한 어노테이션 추가
    - `@Controller`, `@Service`, `@Repository` 어노테이션을 추가해 스프링 빈으로 등록한다.

--- Study/Spring/hello-spring/src/main/java/com/hello/hellospring/service/MemberService.java
+++ Study/Spring/hello-spring/src/main/java/com/hello/hellospring/service/MemberService.java
@@ -2,10 +2,12 @@ package com.hello.hellospring.service;
 
 import com.hello.hellospring.domain.Member;
 import com.hello.hellospring.repository.MemberRepository;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class MemberService {
     private final MemberRepository memberRepository;
 

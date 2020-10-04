0b8a5e4e5fb2deb2c6250e42b0bebe0d65079bc8
suhyunsim <cnc1106@naver.com>
2020-10-04 19:36:49 +0900

    [#20] feat: MemberService 회원 조회 시간 측정 추가

--- Study/Spring/hello-spring/src/main/java/com/hello/hellospring/service/MemberService.java
+++ Study/Spring/hello-spring/src/main/java/com/hello/hellospring/service/MemberService.java
@@ -18,9 +18,17 @@ public class MemberService {
 
     //회원가입
     public Long join(Member member) {
-        validateDuplicateMember(member);
-        memberRepository.save(member);
-        return member.getId();
+        long start = System.currentTimeMillis();
+
+        try {
+            validateDuplicateMember(member);
+            memberRepository.save(member);
+            return member.getId();
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("join " + timeMs + "ms");
+        }
     }
 
     //중복회원 검증

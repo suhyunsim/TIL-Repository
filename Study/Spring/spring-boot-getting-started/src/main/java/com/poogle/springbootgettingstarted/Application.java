379be66fbe9d59216ca2d60b117b066874106087
suhyunsim <cnc1106@naver.com>
2020-10-15 23:08:35 +0900

    [#26] refactor: Bean 재정의 시 에러메세지 확인
    - `@ComponentScan`으로 읽히는 Bean이 항상 우선 시 되어야 함

--- Study/Spring/spring-boot-getting-started/src/main/java/com/poogle/springbootgettingstarted/Application.java
+++ Study/Spring/spring-boot-getting-started/src/main/java/com/poogle/springbootgettingstarted/Application.java
@@ -1,8 +1,10 @@
 package com.poogle.springbootgettingstarted;
 
+import com.poogle.springbootstarter.Holoman;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
@@ -11,4 +13,15 @@ public class Application {
         application.setWebApplicationType(WebApplicationType.NONE);
         application.run(args);
     }
+
+    //이렇게 이 플젝에서 Bean을 재정의하면 에러메세지가 뜸
+    /*
+    @Bean
+    public Holoman holoman() {
+        Holoman holoman = new Holoman();
+        holoman.setName("k");
+        holoman.setHowLong(60);
+        return holoman;
+    }
+    */
 }
